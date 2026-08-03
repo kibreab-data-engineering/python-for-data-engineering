4b8d8886894f9fdb46e4feb819550d15d1717662
kibreab-data-engineering <kfiseha08@gmail.com>
2026-08-03 19:27:57 -0400

    Comiit

---
+++ 02_operation/arithmetic.py
@@ -0,0 +1,17 @@
+# num1 = float(input("Enter first number:\n"))
+# num2 = float(input("Enter second number:\n"))
+
+# print(f"-----------base calculation -----------\nAddition : {num1 + num2}\nSubstration: {num1-num2}\
+#       \nMultiplication: {num1 * num2}\nDivision: {num1/num2}\n-------------------------------------")
+
+# practice Modulus 
+# number = int(input("Enter a number: "))
+# print(f"Remainder when divided by 2 is  {number / 2}")
+# print(f"Remainder when divided by 5 is  {number / 5}")
+
+
+#practice Exponent 
+base = int(input("Base: "))
+power = int(input("Power: "))
+
+print(base ** power)
